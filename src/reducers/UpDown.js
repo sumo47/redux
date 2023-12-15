6af807f5755c75d62d730cb469293cb8cd3e3247
sumit kumar <sumokhan76342@gmail.com>
2023-12-15 20:15:19 +0530

    new brnach

---
+++ src/reducers/UpDown.js
@@ -0,0 +1,9 @@
+const changeTheNumber = (state = initialstate, action)=>{
+    switch(action.type){
+        case "INCREMENT" : return state + 1;
+        case "DECREMENT" : return state - 1;
+        default: return state;
+    }
+}
+
+export default changeTheNumber;
